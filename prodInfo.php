46e8fbd10d4ddbb97621ff51358a9a97212d9475
Davaasuren <Davaasuren@Davaasuren-PC>
2013-01-09 14:37:52 +0000

    Added star rating system with bigImage thumbNails

--- prodInfo.php
+++ prodInfo.php
@@ -3,11 +3,47 @@
     <head>
         <title>Product Information &#124; DAVA</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link rel="stylesheet" type="text/css" href="css/style.css"/>
-        <link rel="stylesheet" type="text/css" href="css/productStyle.css"/>
-        <link rel="stylesheet" type="text/css" href="css/pagination.css"/>
-	<link rel="stylesheet" type="text/css" href="css/grey.css"/>
-        <link rel="stylesheet" type="text/css" href="css/prodInfoStyle.css"/>
+        <link href="css/style.css" rel="stylesheet" type="text/css" />
+        <link href="css/prodInfoStyle.css" rel="stylesheet" type="text/css" />
+        <link href="ratingfiles/ratings.css" rel="stylesheet" type="text/css" />
+        <script src="ratingfiles/ratings.js" type="text/javascript"></script>
+        <script type="text/javascript" src="javascript/jquery-1.8.3.min.js"></script>
+        <script type="text/javascript" src="javascript/jquery.cycle.all.js"></script>
+        <script type="text/javascript" src="javascript/jquery.easing.1.3.js"></script>
+        
+         <script type="text/javascript">
+             $.fn.cycle.updateActivePagerLink = function(pager, currSlideIndex) {
+                 $(pager).find('li').removeClass('activeLI')
+                 .filter('li:eq('+currSlideIndex+')').addClass('activeLI');
+             };
+             
+             $.fn.cycle.updateActivePagerLink = function(pager, currSlide, clsName) {
+                 $(pager).each(function() {
+                     console.log(clsName + '; ' + currSlide ); 
+                     $(this).children().removeClass('activeLI').filter(':eq('+currSlide+')').addClass('activeLI');
+                 });
+             };
+             
+            $(document).ready(
+                function()
+                {
+                    $('#prodSlides').cycle(
+                        {
+                            fx:     'fade',
+                            speed:  'slow',
+                            timeout: 0,
+                            pager: '#prodThumbs',
+                            pagerAnchorBuilder: function(idx, slide) 
+                            {
+                                return '<li><a href="#"><img src="' + slide.src + '" width="50" height="50" /></a></li>';
+                            }
+                            
+                        }
+                    );       
+                }
+            )
+        </script>
+        
     </head>
     <body>
         <div id="container">
@@ -33,18 +69,19 @@
             </div>
             </form>
             
-            <div id="productPanel">
+            <div id="prodInfoDiv">
                 <?php
                 include_once ("connect.php");
                 $type = $_GET["type"];
                 $query = "SELECT * FROM products where type=\"$type\"";
                 $resultSet = mysql_query($query);
-                if (!$resultSet) die("<ERROR: Cannot execute $query>");
-                
+                if (!$resultSet)
+                    die("<ERROR: Cannot execute $query>");
+
                 $prodId = $_GET["prodId"];
                 $prodFound = false;
                 $fetchedRow = mysql_fetch_row($resultSet);
-                
+
                 while ($fetchedRow != null && !$prodFound)
                 {
                     $id = $fetchedRow[0];
@@ -53,33 +90,54 @@
                         $name = $fetchedRow[1];
                         $prodDesc = $fetchedRow[3];
                         $price = $fetchedRow[5];
-                        $bigImageName = $fetchedRow[6];
-                        
-                        echo "<div id=\"prodImage\">";
-                        echo "<h2>$name</h2>";
-                        echo "<img src=\"css/images/chairs/$bigImageName\" alt=\"Big chest image\"/>";
-                        echo "</div>";
-                        
-                        echo "<div id=\"prodDesc\">";
-                        echo "<h2>Product Description</h2>";
-                        echo "<p>$prodDesc</p>";
-                        echo " $price";
-                        echo "</div>";
+                        $BigImgNames = $fetchedRow[6];
+                        $bigImgNamesArray = explode(":", $BigImgNames);
+                        $size = sizeof($bigImgNamesArray);
                         $prodFound = true;
+                        echo "  <hr class='thickLine'/>
+                                <div id='imgDivBg'>
+                                    <div id='imgDiv'>
+                                        <h2>$name</h2>
+                                        <div id='stars'> <div class='srtgs' id='rt_$id'></div> </div>
+                                        <div id='prodSlides'>";
+                        for ($i = 0; $i < $size; $i++)
+                        {
+                            echo "<img src='css/images/chairs/$bigImgNamesArray[$i]' alt='Big chair image'/>";
+                        }
+                        echo "          </div>
+                                        <ul id='prodThumbs'></ul>
+                                    </div>
+                                </div>";
+                        $descArray = explode("!!stop!!", $prodDesc);
+                        echo "$descArray[0]";
+                        echo "<form id='frmAddBasket' action='basket.php'>
+                              <input type='hidden' name='hidId' value='$id'/>
+                              <input type='hidden' name='hidName' value='$name'/>
+                              <input type='hidden' name='hidPrice' value='$price'/>
+                            <p>GBP &pound;$price
+                                <input type='text' name='txtQty' value='1'/>
+                                <input type='submit' name='btnAddToBasket' value='Add to Basket'></p>
+                            </form>";
+                        echo "$descArray[1]";
                     }
                     $fetchedRow = mysql_fetch_row($resultSet);
                 }
-                if ( !$prodFound )
+                if (!$prodFound)
                 {
                     echo "<div id=\"notFound\">";
                     echo "<p>Sorry we couldn't find the item you are looking for, maybe it is no longer available.</p>";
                     echo "</div>";
                 }
                 ?>
-            </div> <!--    end of product Panel   -->
+            </div> <!--    end of product info Panel   -->
             
             
             
+
+   
+   
+   
+   
             <div id="footer">
                 <p>
                     <a href="#">Page Last Updated: December 31, 2012</a>
@@ -93,31 +151,6 @@
                     <a href="#">&copy;2013 All Rights Reserved.</a>
                 </p>
             </div>
-            
-
-    <h2>Details and Dimensions</h2>
-    <ul>
-        <li>Eco-friendly construction</li>
-        <li>Certified sustainable, kiln-dried solid maple and oak hardwood frame</li>
-        <li>Tight seat and back with soy-based polyfoam and fiber cushioning</li>
-        <li>Sinuous wire spring suspension</li>
-        <li>Hardwood legs with a Deco finish</li>
-        <li>100% polyester upholstery</li>
-        <li>Self-welt detailing</li>
-        <li>Benchmade</li>
-        <li>Made in Virginia, USA</li>
-    </ul>
-
-    <h2>Overall Dimensions:</h2>
-    <ul>
-        <li>Width: 30.5&quot; </li>
-        <li>Depth: 28&quot;</li> 
-        <li>Height: 30.5&quot;</li> 
-        <li>SeatDepth: 22&quot;</li> 
-        <li>Height: 19.25&quot;</li>
-    </ul>
-            
-            
         </div> <!--    end of container   -->
     </body>
 </html>
